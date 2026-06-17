7ac8bba470aa831ff9f49ce85a730410c44658ea
Chirag21-dev <chiragks21@outlook.com>
2026-06-17 13:09:19 +0530

    Add VPC module configuration in Main.tf

---
+++ Modules/vpc/Main.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name = "${var.project}-${var.env}-vpc"
+  cidr = var.vpc_cidr
+
+  azs              = ["${var.region}a", "${var.region}b"]
+  public_subnets   = var.public_subnet_cidrs
+  
+
+  enable_nat_gateway           = true
+  single_nat_gateway           = true
+  enable_dns_hostnames         = true
+  enable_dns_support           = true
+  create_database_subnet_group = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  tags = {
+    Project = var.project
+    Env     = var.env
+  }
+}
